05c439a13d573da2639476c33780b84f0c919c46
igorkcarneiro <igorcarneiros@pm.me>
2024-01-22 21:53:45 -0300

    added usage

--- dmarc_checker.sh
+++ dmarc_checker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
+    echo "Usage: $0 <filename>"
     echo "Please provide the filename containing the list of domains as an argument."
     exit 1
 fi
--- subtakeover.sh
+++ subtakeover.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
+    echo "Usage: $0 <filename>"
     echo "Please provide the filename as an argument."
     exit 1
 fi
